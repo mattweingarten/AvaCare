8892dd6213b3b49a1464c657974e11f1a923c437
michaelkeller <michaeke@ethz.ch>
2018-09-15 18:20:01 +0200

    update

--- AvaCare/app/src/main/java/com/example/florian/avacare/SkinsActivity.java
+++ AvaCare/app/src/main/java/com/example/florian/avacare/SkinsActivity.java
@@ -1,5 +1,6 @@
 package com.example.florian.avacare;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SkinsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skins);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
