29fda48c9fbd4eb67be6ed33c8b07d3711798a3e
Matt Weingarten <matt@weingarten.org>
2018-09-16 05:05:40 +0200

    deleted repository inside repository

--- AvaCare/app/src/main/java/com/example/florian/avacare/Messagecallbackhandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.florian.avacare;
-
-import android.support.v7.app.AppCompatActivity;
-
-public abstract class Messagecallbackhandler extends AppCompatActivity{
-
-    public abstract void handleMessageResponse(String s);
-
-}
